00a87d885b5d94cab70020ee86085afd89f5aa14
Hugo Linder <hugoli@kth.se>
2020-12-20 21:29:35 +0100

    model view controller setup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const Datastore = require('nedb')
 const app = express()
 const port = process.env.PORT || 3000
 app.listen(port, () => {
@@ -9,147 +8,31 @@ app.listen(port, () => {
 app.use(express.static('public')) // files available to client
 app.use(express.json({ limit: '1mb' })) // understand incoming data (with 'appliaction/json' header) as JSON
 // in memory is probably sufficient for dev
-const db = {}
-db.players = new Datastore()
-db.gifts = new Datastore()
-// serve player key
-var playerCount = 0
-app.get('/player', function (request, response) {
-  console.log('serving player key')
-  var doc = {
-    _id: playerCount,
-    isConnected: true,
-    timeSinceLastCall: 0,
-    name: 'noName',
-    game: 0
-  }
-  db.players.insert(doc)
-  var giftDoc = {
-    _id: playerCount,
-    sender: playerCount,
-    holder: 'table',
-    position: [0, 0],
-    description: `gift from player ${playerCount}`,
-    game: 0
-  }
-  db.gifts.insert(giftDoc)
-  response.json({ player: playerCount })
-  playerCount = playerCount + 1
-})
-
-app.post('/call', (request, response) => {
-  var player = request.body.player
-  console.log(`Call from player ${player}`)
-  db.players.update({ _id: player }, { $set: {timeSinceLastCall: 0 } })
-  response.send('hi')
-})
-
-// counter force to detect inactive players
-var timeStep = 3000
-var activeThreshold = 3 * timeStep
-setInterval(function () {
-  console.log('Filtering players who do not call.')
-  var options = {multi: true}
-  db.players.update({}, { $inc: { timeSinceLastCall: timeStep } }, options)
-  db.players.update({ timeSinceLastCall: { $gt: activeThreshold } }, { $set: { isConnected: false } }, options)
-  db.players.find({ isConnected: false }, (err, docs) => {
-    if (err) {
-      console.error(err)
-    }
-    console.log('inactive players')
-    console.log(docs.map(x => x._id))
-  })
-}, timeStep)
-
-// roll the dice if it is the players turn
-var round = 0
-var turn = 0
-function nextPlayer (turn) {
-  console.log(`finding the next player at turn ${turn}`)
-  db.players.find({ isConnected: true, _id: { $gt: turn } }, function (err, docs) {
-    if (err) {
-      console.error(err)
-    }
-    console.log('active players:')
-    console.log(docs)
-    if (docs.length === 0) {
-      return 0
-    } else {
-      var possibleTurns = docs.map(x => x._id)
-      console.log('possible turns')
-      console.log(possibleTurns)
-      var closestTurn = Math.min(possibleTurns)
-      console.log(`the closest turn is ${closestTurn}`)
-      return closestTurn
-    }
-  })
+// Using Model-View-Controller
+// The view is client-side, and requires access to the model
+var model = {
+  receivedCommands: [],
+  diceValues: [],
+  players: {},
+  gifts: []
 }
-function currentPlayer (turn) {
-  console.log(`finding current player at turn ${turn}`)
-  // check that the player is still active
-  db.players.find({ isConnected: true, _id: turn }, function (err, docs) {
-    if (err) {
-      console.error(err)
-    }
-    if (docs.length === 0) {
-      return nextPlayer(turn)
-    }
-  })
-  return turn
-}
-setInterval(() => console.log(`at turn ${turn} the current player is ${currentPlayer(turn)}`), 3000)
-
-var diceRoll
-app.post('/roll', function (request, response) {
-  // check permission
-  var player = request.body.player
-  console.log(`dice request from player ${player}`)
-  var current = currentPlayer(turn)
-  console.log(`current player is ${current}`)
-  var message = {}
-  if (player === current) {
-    var maxRoll = 6
-    diceRoll = Math.ceil(Math.random() * maxRoll)
-    message.roll = diceRoll
-    message.success = diceRoll === maxRoll
-  } else {
-    message.succes = false
-    message.note = `It is not your turn to roll. The turn is with player ${current}.`
+app.get('/model', (req, res) => {
+  console.log('serving the model to the client')
+  res.json(model)
+})
+// controller
+app.post('/controller', (req, res) => {
+  console.log('received controller command')
+  var command = req.body
+  // update model following command
+  console.log('updating model')
+  if (command.rollDice) {
+    var diceValue = Math.ceil(Math.random() * 6)
+    model.diceValues.push(diceValue)
   }
-  response.json(message)
-})
-// If the player rolls the die successfully, then they get to pick a present. The package grabbing input is posted for sharing here.
-app.post('/moveGift', function (request, response) {
-})
-// once a player has gotten the present, that is assed to the database and it is the next players turn.
-app.post('/takeGift', function (request, response) {
-  var options = {}
-  db.gifts.update({ _id: request.body.gift }, { $set: { holder: request.body.gift } }, options)
-  turn = nextPlayer(turn)
-  response.send('The gift is yours')
-})
-// count a number, incremented by all clients
-var count = 0
-app.get('/count', function (request, response) {
-  count = count + 1
-  const message = `The server is counting, the button  has been clicked ${count} times across all clients.`
-  console.log('Responding')
-  console.log(message)
-  response.json(message)
-})
+  if (command.playerName) {
 
-/*
-setInterval(function () {
-  db.players.find({}, (err, docs) => {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log(docs)
-    }
-  })
-}, 4000)
-*/
-
-// save to database
-
-// authentication
+  }
+  model.receivedCommands.push(command)
+  res.send('model updated')
+})
--- public/index.js
+++ public/index.js
@@ -1,59 +1,58 @@
-var button = document.getElementById('rollDice')
-async function rollDice (player) {
-  console.log(`Rolling dice as player ${player}`)
+// controls
+var command = {}
+function sendCommand () {
+  console.log('sending command')
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ player: player })
+    body: JSON.stringify(command)
   }
-  const response = await fetch('/roll', options)
-  const data = await response.json()
-  console.log(data)
-}
-
-async function callServer (player) {
-  console.log(`Calling server as player ${player}`)
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ player: player })
-  }
-  fetch('/call', options)
+  fetch('/controller', options)
     .then(response => response.text())
-    .then(data => console.log(data))
+    .then(text => console.log(text))
+    .catch(err => console.error(err))
+  command = {} // reset
 }
-
-// get a player ID for server communication
-fetch('/player')
-  .then(response => response.json())
-  .then(data => {
-    var player = data.player
-    button.addEventListener('click', event => rollDice(player))
-    // Tell the server that this client/player ID is active at time intervals
-    // The response could also inform if it is this client's turn
-    setInterval(() => callServer(player), 3000)
-    var notice = `I am player ${player}`
-    console.log(notice)
-  })
-
-// get the game state from the server at time intervals
-// The game state includes the location of each present, the round and turn in the round, etc.
-// suggestion:
-// round k turn j
-// diceValue n
-// table
-// player presents
-// ID presentID1, presentID2, presentID3
-// ...
-// end table
-
-// Large resources such as images should probaböy be kept separate from the game state
-// I do not want to send several images every second
-
-// draw the game state, possibly using additional resources
-
-// add ability to attempts to make a move (ask the server if it is your turn)
+var diceBtn = document.getElementById('diceBtn')
+diceBtn.addEventListener('click', () => {
+  command.rollDice = true
+  diceBtn.textContent = 'Rolling...'
+})
+var playerBtn = document.getElementById('playerBtn')
+playerBtn.addEventListener('click', () => {
+  command.playerName = document.getElementById('name').value
+  playerBtn.textContent = `${command.playerName}...`
+})
+var giftBtn = document.getElementById('giftBtn')
+giftBtn.addEventListener('click', () => {
+  command.giftLabel = document.getElementById('gift').value
+  giftBtn.textContent = 'Packaging...'
+})
+// view
+function updateView () {
+  console.log('updating view')
+  // get model from server
+  window.fetch('/model')
+    .then(response => response.json())
+    .then(model => {
+      // document.getElementById('modelText').textContent = JSON.stringify(model)
+      document.getElementById('diceLabel').textContent = model.diceValues.pop()
+      diceBtn.textContent = 'Roll the Dice'
+      playerBtn.textContent = 'Create player'
+      giftBtn.textContent = 'Add gift'
+    })
+  document.getElementById('commandText').textContent = JSON.stringify(command)
+}
+// game loop
+var timerId
+document.getElementById('submitInterval').addEventListener('click', () => {
+  if (timerId) {
+    clearInterval(timerId)
+  }
+  timerId = setInterval(function () {
+    sendCommand()
+    updateView()
+  }, document.getElementById('interval').value)
+})
